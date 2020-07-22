2b23863eefd89235f142016330e6fbf7faceface
scottpg <scottpgriffith@gmail.com>
2020-07-22 11:22:49 -0400

    user follows component tree

--- client/src/components/Main.jsx
+++ client/src/components/Main.jsx
@@ -1,7 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Route, Switch, Redirect } from "react-router-dom";
 
-const Main = () => {
-  return <main>Test</main>;
+const Main = ({ user: propUser, handleLogIn, handleRegister }) => {
+  const [user, setUser] = useState(propUser);
+
+  useEffect(() => setUser(propUser), [propUser]);
+
+  return (
+    <main>
+      {/* <Switch>
+        <Route exact path="/" render={() => <Home user={user} />} />
+        <Route
+          exact
+          path="/signin"
+          render={() => (user ? <Redirect to="/" /> : <SignIn />)}
+        />
+        <Route
+          exact
+          path="/signup"
+          render={() => (user ? <Redirect to="/" /> : <SignUp />)}
+        />
+        <Route exact path="/events/:id" render={() => <Event user={user} />} />
+        <Route exact path="/events" render={() => <Events user={user} />} />
+      </Switch> */}
+    </main>
+  );
 };
 
 export default Main;

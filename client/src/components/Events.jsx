6cd29033f98c9bd3fd75c940077bd4fb9a44234f
scottpg <scottpgriffith@gmail.com>
2020-07-22 16:40:31 -0400

    Tested QR Code generation, currently works

---
+++ client/src/components/Events.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Events = () => {
+  return <div>Events Test</div>;
+};
+
+export default Events;
--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,5 +1,26 @@
 import React from "react";
+import QRCode from "qrcode.react";
+import test from "../assets/purple-t-rex.png";
 
-const Home = () => <div>Home Page Placeholder</div>;
+const Home = () => {
+  const logoSettings = {
+    src: test,
+    height: 64,
+    width: 64,
+  };
+
+  return (
+    <>
+      <div>Home Page Placeholder</div>
+      <QRCode
+        value="https://www.example.com"
+        size={256}
+        level="H"
+        includeMargin={true}
+        imageSettings={logoSettings}
+      />
+    </>
+  );
+};
 
 export default Home;

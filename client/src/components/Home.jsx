b8167e1599905e272b2fe0a63fa91f1d47fd5bf6
scottpg <scottpgriffith@gmail.com>
2020-07-22 23:22:30 -0400

    removed test qrcode from home page

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,26 +1,7 @@
 import React from "react";
-import QRCode from "qrcode.react";
-import purpletrex from "../assets/purple-t-rex.png";
 
-const Home = ({ user }) => {
-  const logoSettings = {
-    src: purpletrex,
-    height: 64,
-    width: 64,
-  };
-
-  return (
-    <>
-      <div>Home Page Placeholder</div>
-      <QRCode
-        value={"https://www.example.com"}
-        size={256}
-        level="H"
-        includeMargin={true}
-        imageSettings={logoSettings}
-      />
-    </>
-  );
+const Home = () => {
+  return <div>Home Page Placeholder</div>;
 };
 
 export default Home;

3a17926a072f2d2fbb4c636658221574af8c781d
drewhsu86 <bowserhsu@gmail.com>
2020-07-23 13:34:20 -0400

    error handling in route for generate ticket

--- controllers/index.js
+++ controllers/index.js
@@ -315,7 +315,11 @@ const generateTicket = async (req, res) => {
     console.log(event_ID)
 
     // check if name_on_ticket is a valid string 
-    if (!name_on_ticket || typeof name_on_ticket !== "string" || name_on_ticket.length < 1) {
+    if (!name_on_ticket) {
+      name_on_ticket = userInfo.user_real_name 
+    }
+
+    if (typeof name_on_ticket !== "string" || name_on_ticket.length < 1) {
       name_on_ticket = userInfo.user_real_name 
     }
 

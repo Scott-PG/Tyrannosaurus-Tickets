316b6c29b30a0b10afafce2d25eac5fa95047053
scottpg <scottpgriffith@gmail.com>
2020-07-23 16:24:52 -0400

    Events Page styled

--- client/src/components/shared/Nav.jsx
+++ client/src/components/shared/Nav.jsx
@@ -2,58 +2,48 @@ import React from "react";
 import "./Layout.css";
 import { useHistory, Link } from "react-router-dom";
 import logo from "../../assets/t-rex-logo.png";
-import { withRouter } from "react-router-dom"
+import { withRouter } from "react-router-dom";
 
 const Nav = ({ user, handleLogout, location }) => {
   const history = useHistory();
 
-  // we want to not show the logout button if the path is 
-  // /event/:id 
-  // so empty string before /event/ and nonempty string after 
-  const eventSandwich = location.pathname.split('/event/')
+  // we want to not show the logout button if the path is
+  // /event/:id
+  // so empty string before /event/ and nonempty string after
+  const eventSandwich = location.pathname.split("/event/");
 
-  let onEventPage = false 
+  let onEventPage = false;
 
-  if (eventSandwich[0] === '' && eventSandwich[1] !== '') {
-    onEventPage = true 
+  if (eventSandwich[0] === "" && eventSandwich[1] !== "") {
+    onEventPage = true;
   }
 
   const authenticatedOptions = (
     <>
-      <Link className="link" to="/events">
-        Events
-      </Link>
-
-      {
-        onEventPage ? null : (
+      {onEventPage ? null : (
         <button
-          className="nav-logout-button"
+          className="nav-link-button"
           onClick={() => {
             handleLogout();
             history.push("/");
           }}
-        >Logout
+        >
+          Logout
         </button>
-        ) 
-      }
-        
+      )}
     </>
   );
 
   const unauthenticatedOptions = (
     <>
-      <Link className="link" to="/signup">
-        Sign Up
-      </Link>
-      <Link className="link" to="/signin">
-        Sign In
+      <Link to="/signin">
+        <button className="nav-link-button">Sign-In</button>
       </Link>
     </>
   );
 
   return (
     <header>
-      <img className="logo" src={logo} alt="logo" />
       <nav>{user ? authenticatedOptions : unauthenticatedOptions}</nav>
     </header>
   );

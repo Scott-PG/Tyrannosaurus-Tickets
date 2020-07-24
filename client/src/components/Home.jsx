561ee310a7f02e2ef6d17bb2612d38bed77953da
scottpg <scottpgriffith@gmail.com>
2020-07-23 20:48:59 -0400

    Added logo to home page, erased old routes

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,6 +1,9 @@
 import React, { useState } from "react";
+import tRexText from "../assets/t-rex-tickets-text.png";
+import tRexLogo from "../assets/t-rex-line.png";
 import SignIn from "./SignIn";
 import SignUp from "./SignUp";
+import "./Home.css";
 
 const Home = ({ user, setUser }) => {
   const [signInToggle, setSignInToggle] = useState(true);
@@ -15,6 +18,10 @@ const Home = ({ user, setUser }) => {
 
   return (
     <>
+      <div className="home-logo-div">
+        <img className="t-rex-text" src={tRexText} alt="T-Rex Tickets" />
+        <img className="t-rex-logo" src={tRexLogo} alt="" />
+      </div>
       <div className="home-dynamic">
         {!user ? (
           signInToggle ? (
--- client/src/components/Main.jsx
+++ client/src/components/Main.jsx
@@ -1,8 +1,6 @@
 import React from "react";
 import { Route, Switch, Redirect } from "react-router-dom";
 import Home from "./Home";
-import SignIn from "./SignIn";
-import SignUp from "./SignUp";
 import Events from "./Events";
 import Event from "./Event";
 import TicketGeneration from "./TicketGeneration";
@@ -11,18 +9,6 @@ const Main = ({ user, setUser }) => {
   return (
     <main>
       <Switch>
-        <Route
-          path="/signin"
-          render={() =>
-            user ? <Redirect to="/" /> : <SignIn setUser={setUser} />
-          }
-        />
-        <Route
-          path="/signup"
-          render={() =>
-            user ? <Redirect to="/" /> : <SignUp setUser={setUser} />
-          }
-        />
         <Route exact path="/event/:id" render={() => <Event user={user} />} />
         <Route exact path="/events" render={() => <Events user={user} />} />
         <Route
